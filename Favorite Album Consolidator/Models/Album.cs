bab51ea19ddc8198aad4b1cb43208998389ae9e1
Cary Shin <caryshin11@gmail.com>
2026-01-14 18:21:14 -0800

    album info

--- Favorite Album Consolidator/Models/Album.cs
+++ Favorite Album Consolidator/Models/Album.cs
@@ -6,5 +6,8 @@ namespace Favorite_Album_Consolidator.Models
 {
     public class Album
     {
+       public string Artist { get; set; } = "";
+       public string Title { get; set; } = "";
+       public string ImageUrl { get; set; } = "";
     }
 }
